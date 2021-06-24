9af52d36467beaf37db0038994a43b45c88ee322
Jakob Lorz <jakob@lorz.org>
2021-06-24 13:36:32 +0200

    Update relative URL path for github.io hosted page

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,18 @@
 /* eslint-disable */
 
 module.exports = (config, env, helpers, params = defaultParams) => {
+  if (env.production) {
+    config.output.publicPath = "/wellbeing-session-recovery/";
+
+    config.plugins.push(
+      new helpers.webpack.DefinePlugin({
+        "process.env.PUBLIC_PATH": JSON.stringify(
+          config.output.publicPath || "/"
+        ),
+      })
+    );
+  }
+
   const purgecss = require("@fullhuman/postcss-purgecss")({
     // Specify the paths to all of the template files in your project
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
